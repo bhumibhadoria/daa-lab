0698b9d2230258a75104c1a284181a3c97662ca2
BhumiBhadoria <113108322+BhumiBhadoria@users.noreply.github.com>
2024-02-23 14:48:59 +0530

    :construction_worker: Merge Sort

---
+++ exp3.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class exp3 {
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        int[] temp = new int[arr.length];
+        mergeSortHelper(arr, temp, 0, arr.length - 1);
+    }
+
+    private static void mergeSortHelper(int[] arr, int[] temp, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSortHelper(arr, temp, left, mid);
+            mergeSortHelper(arr, temp, mid + 1, right);
+            merge(arr, temp, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int[] temp, int left, int mid, int right) {
+        for (int i = left; i <= right; i++) {
+            temp[i] = arr[i];
+        }
+        
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (temp[i] <= temp[j]) {
+                arr[k++] = temp[i++];
+            } else {
+                arr[k++] = temp[j++];
+            }
+        }
+
+        while (i <= mid) {
+            arr[k++] = temp[i++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        mergeSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
